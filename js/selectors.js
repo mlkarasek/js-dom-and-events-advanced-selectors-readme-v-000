6a0ddc0e50d5a5faf17739f5b0549ca9bae00cf6
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 22:34:03 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('id img:first-child');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list ul: first-child');
+}
